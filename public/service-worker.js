e784d1383dd6db7508959afb184d374fdea24938
Dauphine Burns <dauphineburns@gmail.com>
2021-08-30 18:19:37 -0400

    deleted unnecessary files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,6 @@
 const FILES_TO_CACHE = [
     "/",
-    "/index.html",
-    "/assets/css/style.css",
-    "/assets/js/loadPosts.js",
-    "/assets/images/Angular-icon.png",
-    "/assets/images/React-icon.png",
-    "/assets/images/Vue.js-icon.png",
-    "/manifest.webmanifest",
-    "/favicon.ico",
-    "/assets/images/icons/icon-72x72.png",
-    "/assets/images/icons/icon-96x96.png",
-    "/assets/images/icons/icon-128x128.png",
-    "/assets/images/icons/icon-144x144.png",
-    "/assets/images/icons/icon-152x152.png",
-    "/assets/images/icons/icon-192x192.png",
-    "/assets/images/icons/icon-384x384.png",
-    "/assets/images/icons/icon-512x512.png",
+    
   ];
   
   const CACHE_NAME = "static-cache-v2";
@@ -74,9 +59,7 @@ const FILES_TO_CACHE = [
   
       return;
     }
-  
-    // if the request is not for the API, serve static assets using "offline-first" approach.
-    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
+
     evt.respondWith(
       caches.match(evt.request).then(function(response) {
         return response || fetch(evt.request);
