8e4f329d685a717641950f5bc1decd1955c4685c
Dauphine Burns <dauphineburns@gmail.com>
2021-09-01 11:33:23 -0400

    seeds created

--- seeders/seed.js
+++ seeders/seed.js
@@ -2,6 +2,29 @@ var mongoose = require("mongoose");
 var db = require("../models");
 
 mongoose.connect (
-    process.env.MONGODB_URI \\ "mongodb://localhost/budget",
+    process.env.MONGODB_URI || "mongodb://localhost/budget",
     { useNewUrlParser: true }
 );
+
+var transactionSeed = [
+    {
+        name: "Pet food",
+        value: 50,
+        date: new Date(Date.now())
+    },
+    {
+        name: "Couch",
+        value: 700,
+        date: new Date(Date.now())
+    },
+    {
+        name: "Car",
+        value: 25,000,
+        date: new Date(Date.now())
+    },
+    {
+        name: "Car insurance",
+        value: 300,
+        date: new Date(Date.now())
+    }
+];
