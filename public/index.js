40490ea66637c56a18577ab3ec9b4a16f21ecc18
Dauphine Burns <dauphineburns@gmail.com>
2021-09-04 10:25:13 -0400

    cleaned up code in index

--- public/index.js
+++ public/index.js
@@ -2,17 +2,6 @@ let transactions = [];
 let myChart;
 
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-  .register('service-worker.js')
-  .then((registration) => {
-    console.log('Service Worker Registered');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-  };
-
 fetch("/api/transaction")
   .then(response => {
     return response.json();
